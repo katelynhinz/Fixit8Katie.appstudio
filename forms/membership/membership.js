7e1868f61a40f8370e0cd1811ad785b5d7231809
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-04 12:48:30 -0500

    Update membership.js

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,1 +1,15 @@
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+let memberName = ""
 
+btnFirstName.onclick=function(){
+  let memberName = inptFirstName.value
+ if (members.includes(memberName)) {
+    lblIsMember.hidden = false
+    lblIsMember.textContent = "That user is a member!"
+   } else { 
+    lblIsMember.hidden = false
+    lblIsMember.textContent = "That first name is not a member, we will add it to the array of members"
+    members.push(memberName)
+  }
+  console.log(members)
+}
